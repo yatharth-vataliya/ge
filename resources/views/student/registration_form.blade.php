d1b78a2ae9835ece64c3a5d1aa16b6a0ad597ebf
yatharth-vataliya <yatharthvataliya@gmail.com>
2020-10-29 16:27:26 +0530

    again last bug is fixed

--- resources/views/student/registration_form.blade.php
+++ resources/views/student/registration_form.blade.php
@@ -122,8 +122,7 @@ GE Registration
 					title: 'Something went wrong',
 					showConfirmButton: false
 				});
-				document.querySelector("#program").innerHTML = `<option value=''>--select any program--</option>`;
-				document.querySelector("#course").innerHTML = `<option value=''>--select any course--</option>`;
+				document.querySelector("#department").innerHTML = `<option value=''>--select any program--</option>`;
 			}
 		});
 		document.querySelector("#program").innerHTML = `<option value=''>--select any program--</option>`;
@@ -162,7 +161,5 @@ GE Registration
 			}
 		});
 	}
-
-
 </script>
 @endsection
